f333d3580b1233f21dc10f934db41380d586b7f4
Deepak Aggarwal <daggarwal77@gmail.com>
2017-12-22 15:37:45 +0530

    Recursion Adv

--- 02_recursion/fib.cc
+++ 02_recursion/fib.cc
@@ -11,9 +11,27 @@ int fib(int n){
 }
 
 
+int fibIter(int n){
+    if (n == 0 || n == 1) return n;
+
+    int prev = 1;
+    int superPrev = 0;
+
+    int cur = prev + superPrev;
+
+    for(int i = 2; i <= n; ++i){
+        int tmp = cur;
+        cur = prev + superPrev;
+        superPrev = prev;
+        prev  = tmp;
+    }
+    return cur;
+}
+
+
 int main(){
   int n;
   cin >> n;
-  int ans = fib(n);
+  int ans = fibIter(n);
   cout << ans;   
 }
